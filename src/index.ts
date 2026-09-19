d17e67c5f90bf564918485cb6d827bd8f004eaee
João Luiz <joaoluizmartins6@gmail.com>
2026-09-19 20:29:42 -0300

    refactor: adapt to ts needs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import express from "express";
-import v1Router from "./routes/v1/index.ts";
+import v1Router from "./routes/v1/index";
 
 const app = express();
-const port = process.env.PORT || 5173;
+const port = process?.env?.["PORT"] || 5173;
 
 app.get("/", (_, response) => {
   response.json({ message: "ok" });
 });
 
-app.use(express.json())
+app.use(express.json());
 app.use("/v1", v1Router);
 
 app.listen(port, () => {
