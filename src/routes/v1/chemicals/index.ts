df68041a8c22bca963a0ddf41e4ec2ae6914f6ec
João Luiz <joaoluizmartins6@gmail.com>
2026-09-19 18:49:57 -0300

    feat: add initial logic to `chemicalRouter` endpoint

--- src/routes/v1/chemicals/index.ts
+++ src/routes/v1/chemicals/index.ts
@@ -1,7 +1,38 @@
-import express from "express"
+import express from "express";
+import { prisma } from "../../../../infrastructure/database/orm/prisma/generated/prisma/prismaClient.ts";
+import { type chemicalModel } from "../../../../infrastructure/database/orm/prisma/generated/prisma/models.ts";
 
-export const chemicalRouter = express.Router()
+export const chemicalRouter = express.Router();
 
-chemicalRouter.get("/", (_, response) => {
-    response.json({message: "chemical router working"})
-})
+type NewChemical = Omit<chemicalModel, "id">;
+
+chemicalRouter.post("/", async (request, response) => {
+  const {
+    name,
+    Description,
+    UN_number,
+    status,
+    risk_class_id,
+    create_date,
+    update_date,
+  }: NewChemical = request.body;
+
+  const newChemical = await prisma.chemical.create({
+    data: {
+      name,
+      Description,
+      UN_number,
+      status,
+      risk_class_id,
+      create_date,
+      update_date,
+    },
+  });
+
+  response.json({ data: newChemical });
+});
+
+chemicalRouter.get("/", async (_, response) => {
+  const chemicals = await prisma.chemical.findMany();
+  response.json({ data: chemicals });
+});
