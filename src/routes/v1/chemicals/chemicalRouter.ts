02aaa30506ab1b7929698c0902be0f5604b9d3bf
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 11:39:23 -0300

    feat: create `deactivateChemical` handler

--- src/routes/v1/chemicals/chemicalRouter.ts
+++ src/routes/v1/chemicals/chemicalRouter.ts
@@ -4,11 +4,13 @@ import readChemicals from "../../../application/handlers/chemicals/readChemicals
 import updateChemical from "../../../application/handlers/chemicals/updateChemicals";
 import readChemicalById from "../../../application/handlers/chemicals/readChemicalById";
 import deleteChemical from "../../../application/handlers/chemicals/deleteChemical";
+import deactivateChemical from "../../../application/handlers/chemicals/deactivateChemical";
 
 export const chemicalRouter = express.Router();
 
 chemicalRouter.post("/", createChemical);
 chemicalRouter.get("/", readChemicals);
 chemicalRouter.get("/:id", readChemicalById);
+chemicalRouter.patch("/deactivate/:id", deactivateChemical);
 chemicalRouter.patch("/:id", updateChemical);
 chemicalRouter.delete("/:id", deleteChemical);
