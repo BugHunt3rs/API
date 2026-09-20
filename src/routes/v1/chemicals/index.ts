2f4cd926bb8857fc531fc4bb06877b29b0b40a2b
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 10:40:30 -0300

    fix: add missing handlers to chemical router

--- src/routes/v1/chemicals/index.ts
+++ src/routes/v1/chemicals/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import createChemical from "../../../application/handlers/chemicals/createChemical";
+import readChemicals from "../../../application/handlers/chemicals/readChemicals";
+import updateChemical from "../../../application/handlers/chemicals/updateChemicals";
 
 export const chemicalRouter = express.Router();
 
-chemicalRouter.post("/");
-chemicalRouter.get("/");
-chemicalRouter.patch("/:id");
+chemicalRouter.post("/", createChemical);
+chemicalRouter.get("/", readChemicals);
+chemicalRouter.patch("/:id", updateChemical);
