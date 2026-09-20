bf13c142f31abb0cb2fb8755ef96b1100d1d4572
João Luiz <joaoluizmartins6@gmail.com>
2026-09-19 21:07:17 -0300

    refactor: move read chemicals logic to dedicated handler

--- src/routes/v1/chemicals/index.ts
+++ src/routes/v1/chemicals/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import createChemical from "../../../handlers/chemicals/createChemical";
+import readChemicals from "../../../handlers/chemicals/readChemicals";
+import updateChemical from "../../../handlers/chemicals/updateChemical";
 
 export const chemicalRouter = express.Router();
 
 chemicalRouter.post("/", createChemical);
-
-chemicalRouter.get("/", async (_, response) => {
-  const chemicals = await prisma.chemical.findMany();
-  response.json({ data: chemicals });
-});
+chemicalRouter.get("/", readChemicals);
+chemicalRouter.patch("/:id", updateChemical);
