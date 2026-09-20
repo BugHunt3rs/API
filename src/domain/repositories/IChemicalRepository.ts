283de45077f984966f043d05046a0ad39b39d453
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 11:39:06 -0300

    refactor: change type to pass `create_date` property when deactivating a chemical

--- src/domain/repositories/IChemicalRepository.ts
+++ src/domain/repositories/IChemicalRepository.ts
@@ -4,6 +4,6 @@ export default interface IChemicalRepository {
   create: (chemical: NewChemical) => Promise<Chemical>;
   readAll: () => Promise<Chemical[]>;
   readById: (id: number) => Promise<Chemical | null>;
-  update: (id: number, newData: Partial<NewChemical>) => Promise<Chemical>;
+  update: (id: number, newData: Partial<Omit<Chemical, "id" | "create_date">>) => Promise<Chemical>;
   delete: (id: number) => Promise<Chemical>;
 }
