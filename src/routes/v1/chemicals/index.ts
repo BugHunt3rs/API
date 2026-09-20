c8a95f6e7912cf5476e2f9f4d51acc80e9ea77a5
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 09:35:06 -0300

    chore: remove handlers  - handlers will be created on /application directory

--- src/routes/v1/chemicals/index.ts
+++ src/routes/v1/chemicals/index.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import createChemical from "../../../handlers/chemicals/createChemical";
-import readChemicals from "../../../handlers/chemicals/readChemicals";
-import updateChemical from "../../../handlers/chemicals/updateChemical";
 
 export const chemicalRouter = express.Router();
 
-chemicalRouter.post("/", createChemical);
-chemicalRouter.get("/", readChemicals);
-chemicalRouter.patch("/:id", updateChemical);
+chemicalRouter.post("/");
+chemicalRouter.get("/");
+chemicalRouter.patch("/:id");
