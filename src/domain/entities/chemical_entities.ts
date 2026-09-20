5b1e519d907f0d62723c045baacb043907e82c5e
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 09:16:15 -0300

    refactor: rename `chemical_entities`

--- src/domain/entities/chemical_entities.ts
+++
@@ -1,4 +0,0 @@
-import type { chemical } from "../../../generated/prisma/client";
-
-export type Chemical = chemical;
-export type NewChemical = Omit<Chemical, "id" | "create_date" | "update_date">;
