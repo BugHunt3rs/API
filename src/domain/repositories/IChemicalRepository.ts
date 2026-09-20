be642d812b5b60bb2637c80d576a641bf77e8a6d
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 11:10:38 -0300

    refactor: remove unnecessary null type

--- src/domain/repositories/IChemicalRepository.ts
+++ src/domain/repositories/IChemicalRepository.ts
@@ -4,6 +4,6 @@ export default interface IChemicalRepository {
   create: (chemical: NewChemical) => Promise<Chemical>;
   readAll: () => Promise<Chemical[]>;
   readById: (id: number) => Promise<Chemical | null>;
-  update: (id: number, newData: Partial<NewChemical>) => Promise<Chemical | null>;
-  delete: (id: number) => Promise<Chemical | null>;
+  update: (id: number, newData: Partial<NewChemical>) => Promise<Chemical>;
+  delete: (id: number) => Promise<Chemical>;
 }
