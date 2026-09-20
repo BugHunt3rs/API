fb2065ce2f4448b6b9dba103fa31eab701ebfac7
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 10:57:17 -0300

    feat: create `readChemicalById` handler

---
+++ src/application/handlers/chemicals/readChemicalById.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import isIdValid from "../../../utils/isIdValid";
+import ChemicalRepository from "../../../infrastructure/database/repositories/chemical/ChemicalRepository";
+export default async function readChemicalById(
+  request: Request,
+  response: Response,
+) {
+  const { id } = request.params;
+
+  if (!id || Array.isArray(id) || isNaN(+id) || !Number.isInteger(+id)) {
+    response.status(500).json({ error: "Invalid ID" });
+    return;
+  }
+
+  try {
+    const chemical = await ChemicalRepository.readById(+id);
+
+    response.status(200).json({ data: chemical });
+  } catch (error) {
+    console.error(error);
+  }
+}
