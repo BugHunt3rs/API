208aa77067ff7641d11e62ef5667d6703bfddbc9
João Luiz <joaoluizmartins6@gmail.com>
2026-09-20 10:57:52 -0300

    feat: create `readChemicalById` handler

--- src/routes/v1/chemicals/chemicalRouter.ts
+++ src/routes/v1/chemicals/chemicalRouter.ts
@@ -2,9 +2,11 @@ import express from "express";
 import createChemical from "../../../application/handlers/chemicals/createChemical";
 import readChemicals from "../../../application/handlers/chemicals/readChemicals";
 import updateChemical from "../../../application/handlers/chemicals/updateChemicals";
+import readChemicalById from "../../../application/handlers/chemicals/readChemicalById";
 
 export const chemicalRouter = express.Router();
 
 chemicalRouter.post("/", createChemical);
 chemicalRouter.get("/", readChemicals);
+chemicalRouter.get("/:id", readChemicalById);
 chemicalRouter.patch("/:id", updateChemical);
